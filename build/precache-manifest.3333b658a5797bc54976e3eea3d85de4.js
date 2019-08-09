20845d9b27ffdbae55fee37a3e77fce3401cdebe
Ashwani Yadav <ashyadavash@gmail.com>
2019-08-09 11:04:41 +0530

    added files

---
+++ build/precache-manifest.3333b658a5797bc54976e3eea3d85de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "207fda55682b79e840ff",
+    "url": "/static/css/main.69c05064.chunk.css"
+  },
+  {
+    "revision": "207fda55682b79e840ff",
+    "url": "/static/js/main.ad79b6cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0fd0786ec4b267398df",
+    "url": "/static/css/2.8ef55759.chunk.css"
+  },
+  {
+    "revision": "c0fd0786ec4b267398df",
+    "url": "/static/js/2.ab0e2c99.chunk.js"
+  },
+  {
+    "revision": "8d4231c18a8f0203990f677ead303fdf",
+    "url": "/index.html"
+  }
+];
